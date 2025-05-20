69be56ca3f2881e0c6d6d03496db3d2908965e2f
acr0matic <work.acr0matic@gmail.com>
2025-05-20 16:01:48 +0300

    feat: плавный скролл

--- src/js/layout/common.js
+++ src/js/layout/common.js
@@ -1,1 +1,13 @@
 // history.scrollRestoration = 'manual';
+
+import { gsap } from "gsap";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+ScrollSmoother.create({
+  smooth: 1.25,
+  effects: true,
+  content: document.querySelector('.smooth-content'),
+});
