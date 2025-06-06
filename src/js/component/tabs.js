0a0e1ff90e990c8774089ecd2f70adda1a5d38f5
artfactor-ru <admin@artfactor.ru>
2025-06-06 10:39:44 +0300

    feat: недостающие элементы

--- src/js/component/tabs.js
+++ src/js/component/tabs.js
@@ -1,17 +1,32 @@
-import { Swiper } from "swiper";
-import { EffectFade } from "swiper/modules";
+// import { Swiper } from "swiper";
+// import { EffectFade } from "swiper/modules";
 
-const tabs = document.querySelectorAll('.b-tabs');
+// const tabs = document.querySelectorAll('.b-tabs');
 
-for (const tab of tabs) {
-  const controls = tab.querySelectorAll('.b-tabs__action .button');
+// for (const tab of tabs) {
+//   const controls = tab.querySelectorAll('.b-tabs__action .button');
 
-  for (const [index, control] of controls.entries()) {
-    control.addEventListener('click', () => {
-      const currentActive = tab.querySelector('.b-tabs__action .is-active');
-      if (currentActive) currentActive.classList.remove('is-active');
+//   const slider = new Swiper(tab.querySelector('.swiper'), {
+//     modules: [EffectFade],
+//     slidesPerView: 1,
+//     spaceBetween: 20,
+//     speed: 300,
+//     allowTouchMove: false,
+//     autoHeight: true,
 
-      control.classList.add('is-active');
-    });
-  }
-}
+//     effect: 'fade',
+//     fadeEffect: {
+//       crossFade: true,
+//     },
+//   });
+
+//   for (const [index, control] of controls.entries()) {
+//     control.addEventListener('click', () => {
+//       const currentActive = tab.querySelector('.b-tabs__action .is-active');
+//       if (currentActive) currentActive.classList.remove('is-active');
+
+//       control.classList.add('is-active');
+//       slider.slideTo(index);
+//     });
+//   }
+// }
